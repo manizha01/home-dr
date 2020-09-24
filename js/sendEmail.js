75d2f43bcaca71f4a3a74a0e7d8485c1d65b0cf7
manizha01 <manizha.razban@gmail.com>
2020-09-24 19:13:53 -0400

    first commit

---
+++ js/sendEmail.js
@@ -0,0 +1,25 @@
+
+function sendMail(contactForm) {
+  emailjs.send("gmail", "Harima", {
+    /*"from_name": contactForm.name.value,
+    "from_email": contactForm.email.value,
+    "message": contactForm.message.value */
+
+       from_name: "contactForm.name.value",
+       from_email: "contactForm.email.value",
+       message: "contactForm.message.value",
+       to_name: "Harima"
+       
+    })
+
+       .then(function (response) {
+          console.log('SUCCESS!', response);
+          alert("your email has been send thanks!");
+       }, function (error) {
+          console.log('FAILED...', error);
+          alert("please try again")
+       });
+ 
+    return false;
+ }
+
